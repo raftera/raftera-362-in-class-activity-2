f076063dbd03e77ca28af995b2573958c29073bb
Aaron Rafter <raftera@oregonstate.edu>
2021-05-13 17:01:22 -0700

    initial setup

---
+++ paldindrome.py
@@ -0,0 +1,11 @@
+def user_palindrome():
+    string = input("Enter a string to test if it's a palindrome.")
+    if palindrome(string):
+        print(string + " is a palindrome.")
+    else:
+        print(string + " is not a palindrome.")
+
+
+def palindrome(string):
+    if string == string[::-1]:
+        return true
